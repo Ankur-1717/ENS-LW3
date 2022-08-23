af672aecfac26af8bfe8ccdb1f7b7ce54f6d6687
Ankur Ambar Mishra <82472228+Ankur-1717@users.noreply.github.com>
2022-08-23 20:40:04 +0530

    update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+module.exports = {
+  ...,
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  trailingSlash: true,
+}
